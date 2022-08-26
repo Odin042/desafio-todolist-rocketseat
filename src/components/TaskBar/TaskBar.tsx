df676ddb83cb4cb8a50f0e785bc0e04288b055e5
Guilherme Fernandes <guilherme.sylva92@gmail.com>
2022-08-25 23:07:34 -0300

    Criação da interação no jsx das tarefas

--- src/components/TaskBar/TaskBar.tsx
+++ src/components/TaskBar/TaskBar.tsx
@@ -2,24 +2,39 @@ import React from "react";
 import { Clipboard, PlusCircle } from "phosphor-react";
 
 import styles from "./TaskBar.module.css";
-    
+
+
 
 export const TaskBar = () => {
-  const[NewTask, setNewTask] = React.useState([
-    "Isso é uma task nova"]);
-  const[countTask, setCountTask] = React.useState(0);
+  const [newTask, setNewTask] = React.useState("");
+  const [listTask, setListTask] = React.useState([""]);
+  const [countTask, setCountTask] = React.useState(0);
+  const [checked, setChecked] = React.useState(false);
 
   const handleNewTask = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    
+    if (!newTask) {
+      return alert("Insira um nome para a tarefa");
+    }
+
+    setListTask([...listTask, newTask]);
+    setNewTask("");
+  }
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+
+    setNewTask(inputValue);
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.input}>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
-        <button>
+        <input type="text" placeholder="Adicione uma nova tarefa" onChange={handleInput} />
+        <button
+          onClick={() => handleNewTask}
+        >
           Criar
         </button>
       </div>
@@ -27,21 +42,23 @@ export const TaskBar = () => {
         <p className={styles.taskname}>Tarefas Criadas</p>
         <p className={styles.taskfinish}> Concluidas </p>
       </div>
-      <div className={styles.taskempty}>
-        <Clipboard className={styles.taskicon} size={70} />
-        <strong>Você ainda não tem tarefas cadastradas</strong>
-        <p>Crie tarefas e organize tarefas a fazer</p>
-      </div>
-      <div className={styles.taskbox}>
-        {NewTask.map(task => {
-          <div className={styles.taskcreate}>
-            <input type="checkbox"></input>
+      {listTask ? 
+        listTask.map((task, index) => (
+          <div className={styles.task} key={index}>
+            <div className={styles.taskname}>
+              <input type="checkbox" onChange={() => setChecked(!checked)} />
+              <p>{task}</p>
+            </div>
           </div>
-        })}
-
+        )):
+      <div className={styles.taskempty}>
+            <Clipboard className={styles.taskicon} size={70} />
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <p>Crie tarefas e organize tarefas a fazer</p>
       </div>
-
-      
+      }
     </div>
+     
   );
-};
+}    
+
